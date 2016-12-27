efb0e7a4d482c865670fdf4c3d8058f4b864e80d
12004210@uhi.ac.uk <12004210@uhi.ac.uk>
2016-12-27 11:54:03 +0000

    Initial

--- DataViewer/server.js
+++ DataViewer/server.js
@@ -3,4 +3,4 @@ var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+}).listen(port); 
