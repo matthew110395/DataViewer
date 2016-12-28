38594597abdbe5590396f41bc631c765fe52b1a5
12004210@uhi.ac.uk <12004210@uhi.ac.uk>
2016-12-28 12:11:18 +0000

    added npm packages

--- DataViewer/typings/index.d.ts
+++ DataViewer/typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/socket.io/index.d.ts" />
+/// <reference path="globals/twitter/index.d.ts" />
